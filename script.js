440a7a392312e762c23e793c2ea5197e1dacb434
Silkoway <ethancai920@gmail.com>
2022-05-19 09:43:32 +0800

    j

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 //string compression from https://raw.githubusercontent.com/pieroxy/lz-string/master/libs/lz-string.min.js
 var LZString = function () { function o(o, r) { if (!t[o]) { t[o] = {}; for (var n = 0; n < o.length; n++)t[o][o.charAt(n)] = n } return t[o][r] } var r = String.fromCharCode, n = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", e = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$", t = {}, i = { compressToBase64: function (o) { if (null == o) return ""; var r = i._compress(o, 6, function (o) { return n.charAt(o) }); switch (r.length % 4) { default: case 0: return r; case 1: return r + "==="; case 2: return r + "=="; case 3: return r + "=" } }, decompressFromBase64: function (r) { return null == r ? "" : "" == r ? null : i._decompress(r.length, 32, function (e) { return o(n, r.charAt(e)) }) }, compressToUTF16: function (o) { return null == o ? "" : i._compress(o, 15, function (o) { return r(o + 32) }) + " " }, decompressFromUTF16: function (o) { return null == o ? "" : "" == o ? null : i._decompress(o.length, 16384, function (r) { return o.charCodeAt(r) - 32 }) }, compressToUint8Array: function (o) { for (var r = i.compress(o), n = new Uint8Array(2 * r.length), e = 0, t = r.length; t > e; e++) { var s = r.charCodeAt(e); n[2 * e] = s >>> 8, n[2 * e + 1] = s % 256 } return n }, decompressFromUint8Array: function (o) { if (null === o || void 0 === o) return i.decompress(o); for (var n = new Array(o.length / 2), e = 0, t = n.length; t > e; e++)n[e] = 256 * o[2 * e] + o[2 * e + 1]; var s = []; return n.forEach(function (o) { s.push(r(o)) }), i.decompress(s.join("")) }, compressToEncodedURIComponent: function (o) { return null == o ? "" : i._compress(o, 6, function (o) { return e.charAt(o) }) }, decompressFromEncodedURIComponent: function (r) { return null == r ? "" : "" == r ? null : (r = r.replace(/ /g, "+"), i._decompress(r.length, 32, function (n) { return o(e, r.charAt(n)) })) }, compress: function (o) { return i._compress(o, 16, function (o) { return r(o) }) }, _compress: function (o, r, n) { if (null == o) return ""; var e, t, i, s = {}, p = {}, u = "", c = "", a = "", l = 2, f = 3, h = 2, d = [], m = 0, v = 0; for (i = 0; i < o.length; i += 1)if (u = o.charAt(i), Object.prototype.hasOwnProperty.call(s, u) || (s[u] = f++, p[u] = !0), c = a + u, Object.prototype.hasOwnProperty.call(s, c)) a = c; else { if (Object.prototype.hasOwnProperty.call(p, a)) { if (a.charCodeAt(0) < 256) { for (e = 0; h > e; e++)m <<= 1, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++; for (t = a.charCodeAt(0), e = 0; 8 > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1 } else { for (t = 1, e = 0; h > e; e++)m = m << 1 | t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t = 0; for (t = a.charCodeAt(0), e = 0; 16 > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1 } l--, 0 == l && (l = Math.pow(2, h), h++), delete p[a] } else for (t = s[a], e = 0; h > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1; l--, 0 == l && (l = Math.pow(2, h), h++), s[c] = f++, a = String(u) } if ("" !== a) { if (Object.prototype.hasOwnProperty.call(p, a)) { if (a.charCodeAt(0) < 256) { for (e = 0; h > e; e++)m <<= 1, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++; for (t = a.charCodeAt(0), e = 0; 8 > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1 } else { for (t = 1, e = 0; h > e; e++)m = m << 1 | t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t = 0; for (t = a.charCodeAt(0), e = 0; 16 > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1 } l--, 0 == l && (l = Math.pow(2, h), h++), delete p[a] } else for (t = s[a], e = 0; h > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1; l--, 0 == l && (l = Math.pow(2, h), h++) } for (t = 2, e = 0; h > e; e++)m = m << 1 | 1 & t, v == r - 1 ? (v = 0, d.push(n(m)), m = 0) : v++, t >>= 1; for (; ;) { if (m <<= 1, v == r - 1) { d.push(n(m)); break } v++ } return d.join("") }, decompress: function (o) { return null == o ? "" : "" == o ? null : i._decompress(o.length, 32768, function (r) { return o.charCodeAt(r) }) }, _decompress: function (o, n, e) { var t, i, s, p, u, c, a, l, f = [], h = 4, d = 4, m = 3, v = "", w = [], A = { val: e(0), position: n, index: 1 }; for (i = 0; 3 > i; i += 1)f[i] = i; for (p = 0, c = Math.pow(2, 2), a = 1; a != c;)u = A.val & A.position, A.position >>= 1, 0 == A.position && (A.position = n, A.val = e(A.index++)), p |= (u > 0 ? 1 : 0) * a, a <<= 1; switch (t = p) { case 0: for (p = 0, c = Math.pow(2, 8), a = 1; a != c;)u = A.val & A.position, A.position >>= 1, 0 == A.position && (A.position = n, A.val = e(A.index++)), p |= (u > 0 ? 1 : 0) * a, a <<= 1; l = r(p); break; case 1: for (p = 0, c = Math.pow(2, 16), a = 1; a != c;)u = A.val & A.position, A.position >>= 1, 0 == A.position && (A.position = n, A.val = e(A.index++)), p |= (u > 0 ? 1 : 0) * a, a <<= 1; l = r(p); break; case 2: return "" }for (f[3] = l, s = l, w.push(l); ;) { if (A.index > o) return ""; for (p = 0, c = Math.pow(2, m), a = 1; a != c;)u = A.val & A.position, A.position >>= 1, 0 == A.position && (A.position = n, A.val = e(A.index++)), p |= (u > 0 ? 1 : 0) * a, a <<= 1; switch (l = p) { case 0: for (p = 0, c = Math.pow(2, 8), a = 1; a != c;)u = A.val & A.position, A.position >>= 1, 0 == A.position && (A.position = n, A.val = e(A.index++)), p |= (u > 0 ? 1 : 0) * a, a <<= 1; f[d++] = r(p), l = d - 1, h--; break; case 1: for (p = 0, c = Math.pow(2, 16), a = 1; a != c;)u = A.val & A.position, A.position >>= 1, 0 == A.position && (A.position = n, A.val = e(A.index++)), p |= (u > 0 ? 1 : 0) * a, a <<= 1; f[d++] = r(p), l = d - 1, h--; break; case 2: return w.join("") }if (0 == h && (h = Math.pow(2, m), m++), f[l]) v = f[l]; else { if (l !== d) return null; v = s + s.charAt(0) } w.push(v), f[d++] = s + v.charAt(0), h--, s = v, 0 == h && (h = Math.pow(2, m), m++) } } }; return i }(); "function" == typeof define && define.amd ? define(function () { return LZString }) : "undefined" != typeof module && null != module && (module.exports = LZString);
 
-
 /**
  * Convert absolute CSS numerical values to pixels.
  *
@@ -91,6 +90,11 @@ function escapeRegExp(string) {
 
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp((find), 'g'), replace);
+}
+
+
+const removeSpecials = (input) => {
+    return replaceAll(input, '( |\\(|\\)|\\:)', '')
 }
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
@@ -196,6 +200,8 @@ Game.load = () => {
 
     Game.alerts = []
 
+    Game.cheatMenu = false;
+
     Game.alert = (message) => {
         Game.alerts.forEach(alert => { clearTimeout(alert) })
         f('#alert').classList.add('alert-on');
@@ -235,7 +241,7 @@ Game.load = () => {
             this.requirement = requirement;
             this.effects = effects;
             this.obj = () => {
-                var newObj = c({ tag: 'div', classList: 'upgrade', id: `upgrade-${replaceAll(name, '( |\\(|\\)|\\:)', '')}`, children: [c({ tag: 'img', src: img })] });
+                var newObj = c({ tag: 'div', classList: 'upgrade', id: `upgrade-${removeSpecials(name)}`, children: [c({ tag: 'img', src: img })] });
                 newObj.addEventListener('mouseenter', () => {
                     f('#hover > #hoverheader').setText(name);
                     f('#hover > #hoverpara').setText(desc);
@@ -284,7 +290,7 @@ Game.load = () => {
             this.obj = () => {
                 var thing = c({
                     tag: 'div',
-                    id: `ascension-${replaceAll(name, "( |\\(|\\)|\\:)", '').toLowerCase()}`,
+                    id: `ascension-${removeSpecials(name).toLowerCase()}`,
                     classList: 'ascensionUpgrade',
                     children: [
                         c({
@@ -524,6 +530,8 @@ Game.load = () => {
         ctx.lineWidth = 4;
         ctx.fillText(`+ ${toShortScale(thing2 ? Game.spc * 3 : Game.spc)}`, getMousePos(document.querySelector('#fx'), e).x + (Math.random() - 0.5) * 10, getMousePos(document.querySelector('#fx'), e).y + (Math.random() - 0.5) * 10)
         Game.cpsTexts.push({ content: `+ ${toShortScale(thing2 ? Game.spc * 3 : Game.spc)}`, x: getMousePos(document.querySelector('#fx'), e).x + (Math.random() - 0.5) * 10, y: getMousePos(document.querySelector('#fx'), e).y + (Math.random() - 0.5) * 10, opacity: 1 })
+    
+        
     }
 
 
@@ -533,20 +541,24 @@ Game.load = () => {
         f('#botsdiv').setHtml('')
         Game.bots.forEach((bot, index) => {
             f('#botsdiv').appendChild(bot.obj())
-            f(`#bot-${replaceAll(bot.name, ' ', '')}`).on('click', () => { bot.buyFunc(); Game.updateBots(); })
+            f(`#bot-${removeSpecials(bot.name)}`).on('click', () => { bot.buyFunc(); Game.updateBots(); })
             document.querySelector(`#bot-${replaceAll(bot.name, '( |\\(|\\))', '')}`).addEventListener('mouseenter', () => {
                 f('#hover > #hoverheader').setText(bot.name);
                 f('#hover > #hoverpara').setText(`Generates ${toShortScale(eval(`(Game.bots[${index}].sps + Game.boosts.${replaceAll(bot.name.toLowerCase(), " ", '')}Up) * Game.boosts.${replaceAll(bot.name.toLowerCase(), " ", '')}Mult`))} shapes per second.\nYour ${eval(`Game.${replaceAll(bot.name.toLowerCase(), " ", '')}s`)} ${bot.name.toLowerCase()}s produce a total of ${toShortScale(eval(`(Game.bots[${index}].sps + Game.boosts.${replaceAll(bot.name.toLowerCase(), " ", '')}Up) * ${eval(`Game.${replaceAll(bot.name.toLowerCase(), " ", '')}s`)} * Game.boosts.${replaceAll(bot.name.toLowerCase(), " ", '')}Mult`))} shapes per second.`);
                 f('#hover > #hoverprice').setText(`${toShortScale(getPrice(bot.baseprice, bot.amount()))} Shapes`)
                 document.querySelector('#hover').style.display = 'block';
             })
-            document.querySelector(`#bot-${replaceAll(bot.name, ' ', '')}`).addEventListener('mouseleave', () => {
+            document.querySelector(`#bot-${removeSpecials(bot.name)}`).addEventListener('mouseleave', () => {
                 document.querySelector('#hover').style.display = 'none';
             })
         })
     }
 
     Game.tick = () => {
+
+        if (Game.name.endsWith(' is a dev')) {
+            Game.cheatMenu = true;
+        }
 
         if (Game.shapes !== Game.shapesEarned) {
             Game.hacked = true;
@@ -647,7 +659,7 @@ Game.load = () => {
             Game.availUpgrades.forEach(upgrade => {
                 var up2 = Game.totalUpgrades.filter(a => a.name === upgrade)[0]
                 f('#upgradesdiv').appendChild(up2.obj())
-                f(`#upgrade-${replaceAll(upgrade, '( |\\(|\\)|\\:)', '')}`).on('click', () => {
+                f(`#upgrade-${removeSpecials(upgrade)}`).on('click', () => {
                     if (Game.shapes >= up2.price) {
                         Game.shapes -= up2.price
                         Game.shapesEarned -= up2.price
@@ -662,12 +674,16 @@ Game.load = () => {
         Game.availUpgrades.forEach(upgrade => {
             var up2 = Game.totalUpgrades.filter(a => a.name === upgrade)[0]
             if (Game.shapes >= up2.price) {
-                f(`#upgrade-${replaceAll(up2.name, '( |\\(|\\)|\\:)', '')}`).classList.remove('disabled')
+                f(`#upgrade-${removeSpecials(up2.name)}`).classList.remove('disabled')
             } else {
-                f(`#upgrade-${replaceAll(up2.name, '( |\\(|\\)|\\:)', '')}`).classList.add('disabled')
+                f(`#upgrade-${removeSpecials(up2.name)}`).classList.add('disabled')
             }
         })
         Game.prevUpgrades = Game.availUpgrades
+
+        if (Game.cheatMenu) {
+            $('#cheating').show()
+        }
     }
     Game.slowtick = () => {
 
@@ -799,7 +815,7 @@ Game.load = () => {
             Game.shapes = 0;
 
             Game.bots.forEach(bot => {
-                eval(`Game.${replaceAll(bot.name.toLowerCase(), '( |\\(|\\)|\\:)', '')}s = 0;`)
+                eval(`Game.${removeSpecials(bot.name)}s = 0;`)
 
             })
             Game.boughtUpgrades = []
@@ -821,16 +837,16 @@ Game.load = () => {
         Game.availAscensionUpgrades.forEach(item => {
             if (item.hasUpgrade()) {
                 f('#ascensionups').appendChild(item.obj())
-                document.querySelector(`#ascension-${replaceAll(item.name, "( |\\(|\\)|\\:)", '').toLowerCase()}`).addEventListener('mouseenter', () => {
+                document.querySelector(`#ascension-${removeSpecials(item)}`).addEventListener('mouseenter', () => {
                     f('#hover-ascension > #hoverheader').setText(item.name);
                     f('#hover-ascension > #hoverpara').setHtml(item.desc + `<br>${Game.gottenAscensionUpgrades.includes(item.name) ? 'Bought' : 'Not Bought'}`);
                     f('#hover-ascension > #hoverprice').setText(`${toShortScale(item.price)} Higher Polygons`)
                     document.querySelector('#hover-ascension').style.display = 'block';
                 })
-                document.querySelector(`#ascension-${replaceAll(item.name, "( |\\(|\\)|\\:)", '').toLowerCase()}`).addEventListener('mouseleave', () => {
+                document.querySelector(`#ascension-${removeSpecials(item)}`).addEventListener('mouseleave', () => {
                     document.querySelector('#hover-ascension').style.display = 'none';
                 })
-                f(`#ascension-${replaceAll(item.name, "( |\\(|\\)|\\:)", '').toLowerCase()}`).on('click', () => {
+                f(`#ascension-${removeSpecials(item)}`).on('click', () => {
                     if (Game.higherPolygons >= item.price && !Game.gottenAscensionUpgrades.includes(item.name)) {
                         Game.higherPolygons -= item.price;
 
